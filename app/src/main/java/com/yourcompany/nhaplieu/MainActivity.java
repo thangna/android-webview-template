8f2ddff24e2fabf8010ece244768e3de88753377
thangna <88230649+thangna@users.noreply.github.com>
2025-09-24 21:14:06 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/yourcompany/nhaplieu/MainActivity.java
@@ -0,0 +1,6 @@
+1. Điều hướng đến: app/src/main/java/com/yourcompany/nhaplieu/
+2. "Add file" → "Create new file"
+3. Tên file: MainActivity.java
+4. Copy nội dung MainActivity.java từ phần trước
+5. Commit message: "Add MainActivity.java"
+6. Commit new file
